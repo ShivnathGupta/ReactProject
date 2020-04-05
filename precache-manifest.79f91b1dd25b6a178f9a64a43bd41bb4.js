d2b98101af80ebd7f6c9c69b1b0b90478d9bd07d
Shivnath <Shivnath0001@gmail.com>
2020-04-05 20:04:35 +0530

    Updates

---
+++ precache-manifest.79f91b1dd25b6a178f9a64a43bd41bb4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f76a2ac5bdeae77dfc70907cd4c196",
+    "url": "/ReactProject/index.html"
+  },
+  {
+    "revision": "916300813b495f1633db",
+    "url": "/ReactProject/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "5b211d514f3963b50892",
+    "url": "/ReactProject/static/css/main.3dc6f0c4.chunk.css"
+  },
+  {
+    "revision": "916300813b495f1633db",
+    "url": "/ReactProject/static/js/2.39a34f49.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/ReactProject/static/js/2.39a34f49.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5b211d514f3963b50892",
+    "url": "/ReactProject/static/js/main.04f76d07.chunk.js"
+  },
+  {
+    "revision": "34c33e4ddee5c6397c56",
+    "url": "/ReactProject/static/js/runtime-main.9103c89e.js"
+  },
+  {
+    "revision": "e430101033efff9a294eaafecbac846a",
+    "url": "/ReactProject/static/media/dribbble_1.e4301010.gif"
+  }
+]);
